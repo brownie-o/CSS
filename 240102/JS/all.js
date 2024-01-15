fe4ee200241e642c832024f21cc9f2cb82e3c4f9
brownie-o <amyooxx1024@gmail.com>
2024-01-15 09:46:25 +0800

    parallax

--- 240102/JS/all.js
+++ 240102/JS/all.js
@@ -1,11 +1,11 @@
-$('#reg_btn, #login_btn').on('click', function(){
+$('#reg_btn, #login_btn').on('click', function () {
   $('body, #navbar').css({
     overflow: 'auto',
     'padding-right': 0
   })
 })
 
-$('#race a').on('click', function(){
+$('#race a').on('click', function () {
   $('#race a').removeClass('active')
   $(this).addClass('active')
 })
@@ -17,7 +17,7 @@ const swiper = new Swiper('.swiper', {
   speed: 1000,
   spaceBetween: 15, // 每個item距離15px
   centerSlides: true,
-  autoplay:{
+  autoplay: {
     delay: 5000,
   },
   slidesPerView: 1,
@@ -49,4 +49,53 @@ const swiper = new Swiper('.swiper', {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+})
+
+// 視差效果 
+// 星空背景
+gsap.to('body', {
+  scrollTrigger: {
+    trigger: 'body',
+    start: 'top 0%',
+    end: 'bottom 0%',
+    scrub: 5, // takes 5 second to "catch up" to the scrollbar
+    // markers: true,
+  },
+  backgroundPosition: '50% 100%',
+  ease: 'none',
+})
+
+// 空島
+// 用時間軸控制三個島做進場動畫
+const float_tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: 'body', // body動時就會觸發
+    start: 'top 0%',
+    end: 'bottom 0%',
+    scrub: 5,
+    // markers: true
+  },
+  ease: 'none' // 空島的動畫速率: none
+})
+
+float_tl
+  .from('.float-wrap-01', {
+    left: '-30%',
+  })
+  .from('.float-wrap-02', {
+    right: '-30%'
+  }, '<') // '<' 讓三個動畫起點一致
+  .from('.float-wrap-03', {
+    bottom: '-100%'
+  }, '<') // '<' 讓三個動畫起點一致
+
+// 空島自己上下浮動的動畫
+$('.float-island').each(function (index, island) {
+  gsap.to(island, {
+    y: 50 * (index + 1),
+    duration: 10 * (index + 1),
+    repeat: -1,
+    yoyo: true,
+    ease: 'power1.inOut'
+  })
 })
